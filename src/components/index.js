6c1545b9af0ee54f9a523eab85f68609a4dabaa5
Avo-Keofteian <avokeofteian@gmail.com>
2021-01-02 14:16:09 +0200

    Remove, circle loader with some minor updates

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Circle from './circle';
 import Oval from './oval';
 import TwoDots from './two-dots';
 
 const Loaders = (type, props) => {
     let ourLoaders = {
-        "Circle": <Circle custom={props} />,
         "Oval": <Oval custom={props} />,
         "TwoDots": <TwoDots custom={props} />
     }
